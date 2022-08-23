95d22ac610672ad7718a7724f6687956e12adbe3
valkur5 <89718916+valkur5@users.noreply.github.com>
2022-08-22 21:02:11 -0300

    Update index.php

--- pruebas/index.php
+++ pruebas/index.php
@@ -1,30 +1,35 @@
-﻿<?php
+<?php
+// a partir de acá está la parte de php, donde masomenos podés guiarte de cómo se comporta el código según el boton que se presione
 $resultado='';
-$archivo='basic.c';
+$archivo='basic.c'; //hay un archivo en la librería local de la computadora "host" que se llama basic.c, basicamente es la estructura del código que se tiene que mostrar en el cuadro de edición
+//La variable $archivo guarda el nombre del archivo que se quiere crear/guardar/cargar 
 if (isset($_GET["archivo"])){
     $archivo = $_GET['archivo'];
-    if ($_GET['boton']=='Guardar'){
+    if ($_GET['boton']=='Guardar'){// si se intenta guardar sobre el archivo base, aparecerá la siguiente alerta, exigiendo que se cambie el nombre
 		if($_GET["archivo"]=="basic.c"){
 			echo '<script language="javascript">alert("Antes de guardar elija otro nombre de archivo");</script>';
+		}else {
+        		file_put_contents($archivo,$_GET['texto']);
+			file_put_contents("auxiliar.c",$_GET['texto']);//Se crea un archivo con el nombre que se escogió y se sobreescribe el archivo auxiliar
 		}
-        file_put_contents($archivo,$_GET['texto']);
-		file_put_contents("auxiliar.c",$_GET['texto']);
     }
-}
-$texto=file_get_contents($archivo);
+$texto=file_get_contents($archivo);//La variable $texto guarda todo el código que se programó, es para cuando se desea cargar un código
 if($_GET['boton']=='Compilar'){
-    shell_exec('compilar.bat auxiliar.c');
+    shell_exec('compilar.bat auxiliar.c');//Si se presiona el botón compilar, ejecuta el archivo "compilar.bat" y le pasa el parámetro del archivo a compilar
     $resultado=file_get_contents('resultado.txt');
 }
 if($_GET['boton']=='Ejecutar'){
-    shell_exec('Debug.bat');
+    shell_exec('Debug.bat');//Si se presiona el botón ejecutar, se ejecuta este archivo bat que se encarga de mandar el código a la plaqueta
 }
 ?>
+
+
 <html>
 	<head><title>PRUEBA</title></head>
 <body>
 <h1> Prueba de recolección de archivos </h1>
 <script>
+	//todo acá se encarga de cargar imagenes de la webcam, una por una de manera rápida, simulando un video
 var newImage = "http://localhost/pruebas/webcam/image.jpg"; 
 function updateImage() { 
 	if(newImage.complete) { 
