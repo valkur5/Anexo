fbd5a84415433c11f3254d239255fcf926e9220c
valkur5 <89718916+valkur5@users.noreply.github.com>
2022-09-28 21:31:12 -0300

    Delete pruebas directory

--- pruebas/Debug.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-@rem cd C:\nxp\MCUXpressoIDE_11.3.0_5222\ide\binaries\Scripts
-@rem bootLPCXpresso.cmd winusb
-cd C:\nxp\MCUXpressoIDE_11.3.0_5222\ide\binaries
-crt_emu_cm_redlink -pLPC1769 -vendor=NXP -flash-load-exec "C:\Users\User\Documents\pruebas\SL2_TP1y2\Debug\SL2_TP1y2.axf"
--- pruebas/ejecucion.php
+++
@@ -1,5 +0,0 @@
-<?=
-	shell_exec('compilar.bat auxiliar.c');
-	while(!file_exists("resultado.txt"))sleep(10);
-    
-?>
